1acd1fdfa6e9b5f58980e93de154ef27f7286529
showrov4g <showrovg00@gmail.com>
2024-07-30 00:36:40 +0600

    making mobile menu active with js

---
+++ script.js
@@ -0,0 +1,7 @@
+let sideMenu = document.querySelector("#sideMenu");
+function openMenu(){
+    sideMenu.style.transform = ("translateX(-16rem)")
+}
+function closeMenu(){
+    sideMenu.style.transform = ("translateX(16rem)")
+}
