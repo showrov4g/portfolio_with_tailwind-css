b4b50376c75b1788b6ee15618db9c82f711da56e
showrov4g <showrovg00@gmail.com>
2024-08-03 18:29:11 +0600

    add some javaScript on website

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 let sideMenu = document.querySelector("#sideMenu");
+let navBar = document.querySelector("nav");
+let navLink = document.querySelector('nav ul');
+
 function openMenu(){
     sideMenu.style.transform = ("translateX(-16rem)")
 }
 function closeMenu(){
     sideMenu.style.transform = ("translateX(16rem)")
 }
+window.addEventListener('scroll', ()=>{
+    if(scrollY > 50){
+       navBar. 
+    }
+})
