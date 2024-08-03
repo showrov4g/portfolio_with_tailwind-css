eda59aa37e19951a66f912d83984c89ca8d42a93
showrov4g <showrovg00@gmail.com>
2024-08-03 20:29:09 +0600

    add fontfamily on website

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@ tailwind.config ={
     theme: {
         gridTemplateColumns:{
             'auto': 'repeat(auto-fit, minmax(200px, 1fr))'
+        },
+        fontFamily:{
+            outfit: ["Outfit", "sans-serif"],
+            ovo: ["Ovo", "serif"]
         }
     }
 }
